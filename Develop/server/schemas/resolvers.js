0d650c69b1c88ccfc67729a0b6c8eeac8947afb6
MonaLisaFrank <126209931+RedactdName@users.noreply.github.com>
2023-12-02 13:05:54 -0700

    installing packages, adding files to utils, server, create schemas folder.

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,42 @@
+const { User } = require('../models');
+const { AuthenticationError, signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent,args,context) => {
+        if (context.user) {
+            return User.find({_id: context.user._id});
+
+        }
+       throw AuthenticationError;
+    },
+  },
+  Mutation: {
+    login: async (parent, {email,password}) => {
+        const user = await User.findOne({email});
+        if (!user) {
+            throw AuthenticationError;
+        }
+        const correctPw = await user.isCorrectPassword(password);
+        if (!correctPw) {
+            throw AuthenticationError;
+        }
+        const token = signToken(user);
+        return {token, user};
+      
+    },
+    addUser: async (parent,args) => {
+      const user = await User.create(args);
+        const token = signToken(user);
+        return {token, user};
+    },
+    saveBook: async () => {
+      
+    },
+    removeBook: async () => {
+      
+    },
+  },
+};
+
+module.exports = resolvers;
